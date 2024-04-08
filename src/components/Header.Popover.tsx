8c8a45ba5e7c128ac1ddda3a5b1cc1e91d883bbb
frankdgcapital <96611469+frankdgcapital@users.noreply.github.com>
2024-04-08 22:07:27 +0200

    Update font

--- src/components/Header.Popover.tsx
+++ src/components/Header.Popover.tsx
@@ -34,8 +34,9 @@ const HeaderPopover = ({ icon, title, message, iconSize = 22 }: IPopover) => {
               leaveTo="opacity-0 translate-y-1">
               <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-60">
                 <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
-                  <strong className="text-gray-700 font-medium">{title}</strong>
-                  <div className="mt-2 py-1 text-sm">{message}</div>
+                  <strong className="text-gray-700 font-md">{title}</strong>
+                  <hr />
+                  <div className="py-1 text-sm">{message}</div>
                 </div>
               </Popover.Panel>
             </Transition>
